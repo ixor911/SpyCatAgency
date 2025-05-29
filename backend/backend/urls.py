19c0c816cf950e1ae4979745dda2e2ce5747061b
ixor911 <igorbutenko9111@gmail.com>
2025-05-29 20:38:16 +0200

    old

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("api/", include("api.urls")),
-    path("api2/", include("api2.urls")),
+    # path("api/", include("api.urls")),
+    path("api/", include("api2.urls")),
     path('admin/', admin.site.urls),
 ]
